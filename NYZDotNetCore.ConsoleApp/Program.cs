fa1afa835448a5731500d00d8dd39de9e4f25897
Nyi Ye Zin <nyiyezin11@gmail.com>
2024-04-11 13:15:17 +0630

    [Complete] AdoDotNet CRUD

--- NYZDotNetCore.ConsoleApp/Program.cs
+++ NYZDotNetCore.ConsoleApp/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using NYZDotNetCore.ConsoleApp;
+
+Console.WriteLine("Hello, World!");
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+// adoDotNetExample.Read();
+// adoDotNetExample.Create("title", "author", "content");
+// adoDotNetExample.Update(1002, "update title", "update author", "update content");
+// adoDotNetExample.Delete(1002);
+adoDotNetExample.Edit(1002);
+adoDotNetExample.Edit(1);
+
+Console.ReadKey();
