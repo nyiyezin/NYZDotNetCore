9041d561a33aa8af6fdfd0572981a9299098ad4d
Nyi Ye Zin <nyiyezin11@gmail.com>
2024-05-02 21:46:51 +0630

    [Complete] Dapper Custom Service

---
+++ NYZDotNetCore.Shared/DapperService.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace NYZDotNetCore.Shared
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+
+        public DapperService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<T> Query<T>(string query, object? param = null)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            var list = dbConnection.Query<T>(query, param).ToList();
+            return list;
+        }
+
+        public T QueryFirstOrDefault<T>(string query, object? param = null)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);         
+            var item = dbConnection.Query<T>(query, param).FirstOrDefault();
+            return item!;
+        }
+
+        public int Execute(string query, object? param = null)
+        {
+            using IDbConnection dbConnection = new SqlConnection(_connectionString);
+            var result = dbConnection.Execute(query, param);
+            return result; 
+        }
+    }
+}
