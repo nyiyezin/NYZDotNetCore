261b54e650c28d9091b558d9a515fb09088baffb
Nyi Ye Zin <nyiyezin11@gmail.com>
2024-06-15 18:47:56 +0630

    [Complete] NLayer Architecture

---
+++ NYZDotNetCore.NLayer.DataAccess/Db/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NYZDotNetCore.NLayer.DataAccess.Models;
+
+namespace NYZDotNetCore.NLayer.DataAccess.Db;
+
+public class AppDbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+    }
+
+    public DbSet<BlogModel> Blogs { get; set; }
+}
---
+++ NYZDotNetCore.NLayer.UI/Program.cs
@@ -0,0 +1,5 @@
+﻿using NYZDotNetCore.NLayer.BusinessLogic.Services;
+
+Console.WriteLine("Hello, World!");
+
+BL_Blog bL_Blog = new BL_Blog();
